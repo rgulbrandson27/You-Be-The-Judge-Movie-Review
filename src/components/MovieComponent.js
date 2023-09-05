789d516aee54862e6767fc4b37c5bf57e203a707
rgulbrandson27 <105253984+rgulbrandson27@users.noreply.github.com>
2023-09-05 01:25:25 -0500

    hover effect added

--- src/components/MovieComponent.js
+++ src/components/MovieComponent.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import './MovieComponent.css'
 import movies from './MovieList.js';
 import Scale from './Images/Scale.png';
-import Final from './Images/Final.png';
+import NewViewWhite from './Images/NewViewWhite.png';
 import DescriptionCom from './DescriptionCom.js';
 import reactDOM from 'react';
 import ReactDOM from 'react-dom/client'
@@ -10,17 +10,26 @@ import ReactDOM from 'react-dom/client'
     const MovieComponent = ({ title, poster, description, year, rating, runtime, imbd, count, average}) => {
    
     const[handleClick, setHandleClick] = useState(false);
+    const[handleHover, setHandleHover] = useState(false);
+    const posterStyle = { minWidth: "115%", minHeight: "115%", z:"12"};
 
+    const length = {title}.length;
+    
     return (
         <>
-            {handleClick ?  <DescriptionCom className="description" title={title} description={description}></DescriptionCom>  :
-            <div className="movie">
-                <div className="poster-area">              
-                    <img className="poster" src={poster} />
+            {handleClick ?  <DescriptionCom className="description" title={title} description={description}></DescriptionCom>  
+            : <div className="movie">
+                <div className="poster-area"     
+                    onMouseEnter = {e => setHandleHover(true)}
+                    onMouseLeave = {e => setHandleHover(false)}>      
+                  
+                  {handleHover ? <img className="poster" style={posterStyle} src={poster}/> : <img className="poster" src={poster}/> 
+                  
+                    }
                 </div>   
                 <div className="text-area">
                     <h2 className="title">{title}</h2>
-                        <img className="reel" src={Final}
+                        <img className="reel" src={NewViewWhite}
                             onClick = {e => setHandleClick(true)}/>
                     <p className="link">{imbd}</p>
                     <p className="year">{year}</p>
